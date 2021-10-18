a114ab186d7dbdeabe557e0f998ff4adbcb1ea6e
Sergey Kudashev <kudashevs@gmail.com>
2021-10-18 22:37:39 +0300

    6kyu: "Which are in" solved using filter and loop.

---
+++ katas/Kyu6/Which_are_in/inArray_solution.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Katas\Kyu6\Which_are_in;
+
+function inArray($array1, $array2)
+{
+    return call_user_func(
+        function ($array) {
+            sort($array);
+
+            return $array;
+        },
+        array_filter($array1, function ($needle) use ($array2) {
+            foreach ($array2 as $hay) {
+                if (strpos($hay, $needle) !== false) {
+                    return true;
+                }
+            }
+
+            return false;
+        })
+    );
+}
---
+++ tests/Kyu6/Which_are_in/InArrayTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Katas\Tests\Kyu6\Which_are_in;
+
+use PHPUnit\Framework\TestCase;
+use function Katas\Kyu6\Which_are_in\inArray;
+
+class InArrayTest extends TestCase
+{
+    private function revTest($actual, $expected)
+    {
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testBasics()
+    {
+        $a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
+        $a1 = ["arp", "live", "strong"];
+        $this->revTest(inArray($a1, $a2), ["arp", "live", "strong"]);
+        $a1 = ["xyz", "live", "strong"];
+        $this->revTest(inArray($a1, $a2), ["live", "strong"]);
+        $a1 = ["live", "strong", "arp"];
+        $this->revTest(inArray($a1, $a2), ["arp", "live", "strong"]);
+    }
+}
